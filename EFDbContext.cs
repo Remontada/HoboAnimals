40e30c43b83f490d8d0fa2b30859f035164df0e9
Vit <Remontada@users.noreply.github.com>
2016-04-25 20:24:33 +0300

    Added files via upload

---
+++ EFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HoboAnimal.Domain.Entities;
+using System.Data.Entity;
+
+namespace HoboAnimal.Domain.Concrete
+
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Profile> Profiles { get; set; }
+    }
+}
